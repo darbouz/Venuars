9558744a931f42291a5d607176f0bd536f8a6e2d
darbouz <galaxy.darbouz@gmail.com>
2019-10-16 21:24:05 +0100

    Switch Done

--- Assets/ObjectController.cs
+++ Assets/ObjectController.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class ObjectController : MonoBehaviour
 {
+
+    public bool disabled;
     public float runSpeed = 2f;
     public float jumpVelocity = 5f;
     public PhysicsObject controlledObject;
- 
+
     void Update()
     {
-        controlledObject.targetXVelocity = Input.GetAxisRaw("Horizontal") * runSpeed;
-        if (Input.GetButtonDown("Jump") && controlledObject.isGrounded())
-            controlledObject.setYVelocity(jumpVelocity);        
+        if (!disabled)
+        {
+            controlledObject.targetXVelocity = Input.GetAxisRaw("Horizontal") * runSpeed;
+            if (Input.GetButtonDown("Jump") && controlledObject.isGrounded())
+                controlledObject.setYVelocity(jumpVelocity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            disabled = !disabled;
+            controlledObject.targetXVelocity = 0;
+        }
     }
 }
