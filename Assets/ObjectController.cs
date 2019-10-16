7cf428866154de635b6d3834b76160f24a7bbbeb
darbouz <galaxy.darbouz@gmail.com>
2019-10-16 21:13:29 +0100

    Symetric Done

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public float runSpeed = 2f;
+    public float jumpVelocity = 5f;
+    public PhysicsObject controlledObject;
+ 
+    void Update()
+    {
+        controlledObject.targetXVelocity = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump") && controlledObject.isGrounded())
+            controlledObject.setYVelocity(jumpVelocity);        
+    }
+}
--- Assets/PhysicsObject.cs
+++ Assets/PhysicsObject.cs
@@ -1,32 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PhysicsObject : MonoBehaviour
 {
 
-    public float minGroundNormalY = .65f;
-    public float gravityModifier = 1f;
+    [SerializeField] LayerMask colliders;
+    public enum Direction {Up = -1,Down = 1};
+    public Direction gravityDirection;
 
-    protected Vector2 targetVelocity;
-    protected bool grounded;
-    protected Vector2 groundNormal;
-    protected Rigidbody2D rb2d;
+    public float gravityModifier = 1f;
+    public float targetXVelocity {get;set; }
+    
+    
     protected Vector2 velocity;
+    protected BoxCollider2D boxCollider2D;
+    protected Rigidbody2D rb2d;
     protected ContactFilter2D contactFilter;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
-
-
-    protected const float minMoveDistance = 0.001f;
     protected const float shellRadius = 0.01f;
+
     private void OnEnable()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
-    private void Start()
+    void Start()
     {
+        targetXVelocity = 0;
         contactFilter.useTriggers = false;
         contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
         contactFilter.useLayerMask = true;
@@ -36,53 +39,50 @@ public class PhysicsObject : MonoBehaviour
     {
         
     }
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        velocity += Physics2D.gravity * gravityModifier * Time.fixedDeltaTime;
+        velocity += Physics2D.gravity * gravityModifier * (float)gravityDirection * Time.fixedDeltaTime;
+        velocity.x = targetXVelocity;
         Vector2 deltaPosition = velocity * Time.fixedDeltaTime;
-        Vector2 movementDirection = deltaPosition * Vector2.up;
-
-        move(movementDirection);
+       
+        moveX(deltaPosition * Vector2.right);
+        moveY(deltaPosition * Vector2.up);
     }
 
-    private void move(Vector2 movementDirection)
+    private void moveX(Vector2 movementDirection)
+    {
+        float distance = detectCollision(movementDirection);
+        rb2d.position = rb2d.position + movementDirection.normalized * distance;
+    }
+    private void moveY(Vector2 movementDirection)
+    {
+        float distance = detectCollision(movementDirection);
+        rb2d.position = rb2d.position + movementDirection.normalized * distance;
+    }
+
+    private float detectCollision(Vector2 movementDirection)
     {
         float distance = movementDirection.magnitude;
-        
+        //Check if the next Movement will be a collision with the surface
         int count = rb2d.Cast(movementDirection, contactFilter, hitBuffer, distance + shellRadius);
 
         for (int i = 0; i < count; i++)
         {
+            //Modify the distance to shellRadius
             float modifiedDistance = hitBuffer[i].distance - shellRadius;
             distance = modifiedDistance < distance ? modifiedDistance : distance;
         }
-        
-        rb2d.position = rb2d.position + movementDirection.normalized * distance;
+        return distance;
     }
-    private void move2(Vector2 move,bool yMovement)
+    public bool isGrounded()
     {
-        float distance = move.magnitude;
-        
+        RaycastHit2D castRay = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down * (float)gravityDirection, shellRadius, colliders);
+        return castRay.collider != null;
+    }
 
-        if (distance > minMoveDistance)
-        {
-            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
-            hitBufferList.Clear();
-            for (int i = 0; i < count; i++)
-            {
-                hitBufferList.Add(hitBuffer[i]);
-            }
-
-            for (int i = 0; i < hitBufferList.Count; i++)
-            {
-                float modifiedDistance = hitBufferList[i].distance - shellRadius;
-                distance = modifiedDistance < distance ? modifiedDistance : distance;
-            }
-
-
-        }
-        rb2d.position = rb2d.position + move.normalized * distance;
+    public void setYVelocity(float Yvelocity)
+    {
+        velocity.y = Yvelocity * (float)gravityDirection;
     }
 }
 
